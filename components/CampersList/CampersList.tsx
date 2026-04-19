0ebac416c4dbb1affbf3c9a1283805a94885c691
Olena Petrova <petrovahel@icloud.com>
2026-04-19 15:42:31 +0300

    fix TypeScript error with useInfiniteQuery pages

--- components/CampersList/CampersList.tsx
+++ components/CampersList/CampersList.tsx
@@ -5,7 +5,6 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import CamperCard from "@/components/CamperCard/CamperCard";
 import { Filters } from "@/types/filters";
 import css from "./CampersList.module.css";
-import type { CampersResponse } from "@/types/camper";
 
 type Props = {
   filters: Filters;
@@ -14,19 +13,10 @@ type Props = {
 
 export default function CampersList({ filters }: Props) {
   const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
-    useInfiniteQuery<
-      CampersResponse,
-      Error,
-      CampersResponse,
-      [_: string, Filters],
-      number
-    >({
+    useInfiniteQuery({
       queryKey: ["campers", filters],
-
       queryFn: ({ pageParam = 1 }) => fetchCampers(pageParam, filters),
-
       initialPageParam: 1,
-
       getNextPageParam: (lastPage) =>
         lastPage.page < lastPage.totalPages ? lastPage.page + 1 : undefined,
     });
@@ -38,11 +28,12 @@ export default function CampersList({ filters }: Props) {
       </div>
     );
   }
+  const pages = data?.pages ?? [];
 
   return (
     <section className={css.container}>
       <ul className={css.camperList}>
-        {data?.pages?.flatMap((page) =>
+        {pages.flatMap((page) =>
           page.campers.map((camper) => (
             <CamperCard key={camper.id} camper={camper} />
           ))
